43762a0ad836de5f50d990fc44ba089fe902e339
Tukaku <Primeira Verso>
2020-10-16 18:41:27 -0300

    Primeira Versão

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase : {
-    apiKey: "AIzaSyD-a5_iWdNDRTHBo81MP0ukiMlCTjkbQbI",
-    authDomain: "senac-20195.firebaseapp.com",
-    databaseURL: "https://senac-20195.firebaseio.com",
-    projectId: "senac-20195",
-    storageBucket: "senac-20195.appspot.com",
-    messagingSenderId: "604562411891",
-    appId: "1:604562411891:web:32865d9dcb1fbe2d3ff506"
+    apiKey: "AIzaSyA-AXY6zgvEN_UunKwbmbBc6Ho9qdy314c",
+    authDomain: "projeto-integrador-12b61.firebaseapp.com",
+    databaseURL: "https://projeto-integrador-12b61.firebaseio.com",
+    projectId: "projeto-integrador-12b61",
+    storageBucket: "projeto-integrador-12b61.appspot.com",
+    messagingSenderId: "402942265849",
+    appId: "1:402942265849:web:eaf356d7f04936b2dad528"
   }
 };
